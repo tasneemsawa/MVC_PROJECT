0c68665a92d3f6d50c38fabe7050c92b5c8fa5b0
Tasneem Sawalha <tasneem31997@gmail.com>
2026-05-31 19:19:43 +0300

    First commit - MVC Project

---
+++ MVC_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ASP14_EF;User Id=sa;Password=Password123;TrustServerCertificate=True");
+        }
+
+    }
+}
+
